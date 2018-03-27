589775b71cd19b8040f02e1ed1a6cc9efc544268
me <me@MAIS-JARADAT>
2018-03-28 01:13:03 +0300

    first commit

---
+++ src/seletest/test.java
@@ -0,0 +1,22 @@
+package seletest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class test {
+	static WebDriver driver;
+public	static void main(String argv[]){
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\me\\Downloads\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("mais_jaradat_2000@yahoo.com");
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("123");
+		driver.findElement(By.xpath(".//*[@id='loginbutton']")).click();
+		
+		
+	}
+
+}
